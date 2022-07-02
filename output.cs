e141427196402b0f9522fc278a86c3dea547356f
gerhercar21 <88815809+gerhercar21@users.noreply.github.com>
2022-07-02 12:32:00 -0500

    Animation and About Us page

---
+++ output.cs
@@ -0,0 +1,267 @@
+.blog .title {
+  font-family: "Bebas Neue", cursive;
+  color: #135359;
+}
+
+.blog {
+  background-color: #ffffff;
+  max-width: unset;
+  padding: 5% 10%;
+}
+@media (max-width: 575px) {
+  .blog {
+    padding: 5% 10% 18% 10%;
+  }
+}
+.blog .process-list {
+  margin-top: 40px;
+  margin-bottom: 36px;
+}
+.blog .process-list .process {
+  margin-bottom: 25px;
+}
+.blog .process-list .process > div > h3 {
+  color: #025157;
+  margin-bottom: 0;
+  font-size: 30px;
+  font-weight: 500;
+}
+.blog .process-list .process > div > p {
+  color: #135359;
+  margin: 0;
+  text-align: left;
+}
+.blog .process-list .process .process-icon {
+  width: 60px;
+  height: 60px;
+  margin-right: 10px;
+  text-align: center;
+}
+.blog .process-list .process .process-icon img {
+  height: 100%;
+}
+.blog button {
+  height: auto;
+  border-radius: 8px;
+  padding: 12px;
+  width: 180px;
+  margin-bottom: 15px;
+}
+.blog .card * {
+  text-align: left;
+}
+.blog .card .card-title {
+  color: #4a4a4a;
+  font-size: 24px;
+}
+.blog .card .card-text {
+  color: #4a4a4a;
+  font-size: 18px;
+}
+.blog .card .category {
+  color: #67b54b;
+  text-align: left;
+  font-size: 14px;
+}
+.blog .card .read-time {
+  color: #979797;
+  font-size: 14px;
+}
+.blog .card .author p {
+  color: #4a4a4a;
+  font-size: 14px;
+  margin: 15px 0 16px 8px;
+}
+.blog .card .author img {
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+}
+.blog .card .read-more {
+  text-decoration: underline;
+}
+
+.tryMatcha {
+  background-color: #96d178;
+  padding: 5% 5% 2%;
+  margin-top: -30px;
+}
+.tryMatcha h1 {
+  font-size: 60px;
+}
+@media (max-width: 575px) {
+  .tryMatcha h1 {
+    font-size: 45px;
+  }
+}
+.tryMatcha .signup-form input {
+  padding: 0.5rem 1rem;
+  margin-right: 0;
+  border: 1px solid #b6b6b6;
+  border-radius: 8px;
+  height: 55px;
+  width: 60%;
+  max-width: 250px;
+}
+@media (max-width: 575px) {
+  .tryMatcha .signup-form input {
+    margin-right: -30px;
+  }
+}
+.tryMatcha .signup-form button {
+  margin-bottom: 1rem;
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+  margin-right: 0;
+  margin-left: -12px;
+  padding: 0.8rem 1.2rem;
+  height: 55px;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  font-size: 16px;
+}
+.tryMatcha p {
+  color: #ffffff;
+  font-size: 14px;
+}
+
+.freeaccount {
+  padding: 5% 10% 15%;
+  margin: 0;
+  max-width: unset;
+}
+.freeaccount .box {
+  background-color: #025157;
+  padding: 5%;
+  border-radius: 10px;
+  color: #ffffff;
+}
+.freeaccount .box form + p {
+  color: #b3cbcd;
+  font-size: 14px;
+}
+.freeaccount .col-sm p {
+  text-align: left;
+  font-size: 17px;
+  color: #ffffff;
+}
+@media (max-width: 575px) {
+  .freeaccount .col-sm p {
+    font-size: 14px;
+  }
+}
+@media (min-width: 576px) and (max-width: 767px) {
+  .freeaccount .col-sm p {
+    font-size: 14px;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .freeaccount .col-sm p {
+    font-size: 95%;
+  }
+}
+.freeaccount .col-sm h2 {
+  font-size: 60px;
+  font-family: "Bebas Neue", cursive;
+  line-height: 1;
+}
+@media (max-width: 575px) {
+  .freeaccount .col-sm h2 {
+    font-size: 40px;
+  }
+}
+@media (min-width: 576px) and (max-width: 767px) {
+  .freeaccount .col-sm h2 {
+    font-size: 30px;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .freeaccount .col-sm h2 {
+    font-size: 40px;
+  }
+}
+.freeaccount .col-sm a {
+  color: #67b54b;
+  text-decoration: none;
+}
+@media (max-width: 575px) {
+  .freeaccount .signup-form {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+@media (min-width: 576px) and (max-width: 767px) {
+  .freeaccount .signup-form {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .freeaccount .signup-form {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+.freeaccount .signup-form input {
+  padding: 0.5rem 1rem;
+  margin-right: 0;
+  border: 1px solid #b6b6b6;
+  border-radius: 8px;
+  height: 55px;
+  width: 55%;
+  max-width: 250px;
+}
+@media (max-width: 575px) {
+  .freeaccount .signup-form input {
+    margin-right: -30px;
+    font-size: 90%;
+    padding: 0.2rem 0.7rem;
+    width: 70%;
+  }
+}
+@media (min-width: 576px) and (max-width: 767px) {
+  .freeaccount .signup-form input {
+    margin-right: -30px;
+    font-size: 70%;
+    padding: 0.2rem 0.7rem;
+    width: 70%;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .freeaccount .signup-form input {
+    font-size: 90%;
+  }
+}
+.freeaccount .signup-form button {
+  margin-bottom: 1rem;
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+  margin-right: 0;
+  margin-left: -12px;
+  padding: 0.8rem 1.2rem;
+  height: 55px;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  font-size: 16px;
+  background-color: #67b54b;
+}
+@media (max-width: 575px) {
+  .freeaccount .signup-form button {
+    padding: 0.8rem 0.5rem;
+    font-size: 90%;
+  }
+}
+@media (min-width: 576px) and (max-width: 767px) {
+  .freeaccount .signup-form button {
+    padding: 0.8rem 0.5rem;
+    font-size: 70%;
+    max-width: 100px;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .freeaccount .signup-form button {
+    font-size: 90%;
+  }
+}
+
+/*# sourceMappingURL=output.cs.map */
